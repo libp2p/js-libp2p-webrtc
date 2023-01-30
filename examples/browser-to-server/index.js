334cda3d9249c82bcc969d9430cf0d1faa842f97
David DiMaria <info@collectivesessions.com>
2023-01-30 16:46:35 -0700

    Add playwright tests and add to CI

--- examples/browser-to-server/index.js
+++ examples/browser-to-server/index.js
@@ -9,7 +9,7 @@ import { pushable } from 'it-pushable';
 let stream;
 const output = document.getElementById('output')
 const sendSection = document.getElementById('send-section')
-const appendOutput = (line) => output.innerText += `${line}\n`
+const appendOutput = (line) => output.innerHTML += `<div>${line}</div>`
 const clean = (line) => line.replaceAll('\n', '')
 const sender = pushable()
 
@@ -27,7 +27,7 @@ node.connectionManager.addEventListener('peer:connect', (connection) => {
 
 window.connect.onclick = async () => {
   const ma = multiaddr(window.peer.value)
-  appendOutput(`Dialing ${ma}`)
+  appendOutput(`Dialing '${ma}'`)
   stream = await node.dialProtocol(ma, ['/echo/1.0.0'])
   pipe(sender, stream, async (src) => {
     for await(const buf of src) {
---
+++ examples/browser-to-server/tests/test.js
@@ -0,0 +1,98 @@
+/* eslint-disable no-console */
+import { test, expect } from '@playwright/test'
+import { playwright } from 'test-util-ipfs-example'
+import { spawn, exec } from 'child_process'
+import { existsSync } from 'fs'
+
+// Setup
+const play = test.extend({
+  ...playwright.servers()
+})
+
+async function spinUpGoLibp2p() {
+  if (!existsSync('../../examples/go-libp2p-server/go-libp2p-server')) {
+    await new Promise((resolve, reject) => {
+      exec('go build',
+        { cwd: '../../examples/go-libp2p-server' },
+        (error, stdout, stderr) => {
+          if (error) {
+            reject(error)
+            console.error(`exec error: ${error}`)
+            return
+          }
+          resolve()
+        })
+    })
+  }
+
+  const server = spawn('./go-libp2p-server', [], { cwd: '../../examples/go-libp2p-server', killSignal: 'SIGINT' })
+  server.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`, typeof data)
+  })
+  const serverAddr = await (new Promise(resolve => {
+    server.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`, typeof data)
+      if (data.includes('p2p addr:  ')) {
+        // Parse the addr out
+        resolve((String(data)).match(/p2p addr:  ([^\s]*)/)[1])
+      }
+    })
+  }))
+  return { server, serverAddr }
+}
+
+play.describe('bundle ipfs with parceljs:', () => {
+  // DOM
+  const connectBtn = '#connect'
+  const connectAddr = '#peer'
+  const messageInput = '#message'
+  const sendBtn = '#send'
+  const output = '#output'
+
+  let server
+  let serverAddr
+
+  // eslint-disable-next-line no-empty-pattern
+  play.beforeAll(async ({ }, testInfo) => {
+    testInfo.setTimeout(5 * 60_000)
+    const s = await spinUpGoLibp2p()
+    server = s.server
+    serverAddr = s.serverAddr
+    console.log('Server addr:', serverAddr)
+  }, {})
+
+  play.afterAll(() => {
+    server.kill('SIGINT')
+  })
+
+  play.beforeEach(async ({ servers, page }) => {
+    console.log(`http://localhost:${servers[0].port}/`)
+    await page.goto(`http://localhost:${servers[0].port}/`)
+  })
+
+  play('should connect to a go-libp2p node over webtransport', async ({ page }) => {
+    const message = "hello"
+    
+    // add the go libp2p multiaddress to the input field and submit
+    await page.fill(connectAddr, serverAddr)
+    await page.click(connectBtn)
+
+    // send the relay message to the go libp2p server
+    await page.fill(messageInput, message)
+    await page.click(sendBtn)
+
+    await page.waitForSelector('#output:has(div)')
+
+    // Expected output:
+    //
+    // Dialing '${serverAddr}'
+    // Peer connected '${serverAddr}'
+    // Sending message '${message}'
+    // Received message '${message}'
+    const connections = await page.textContent(output)
+    expect(connections).toContain(`Dialing '${serverAddr}'`)
+    expect(connections).toContain(`Peer connected '${serverAddr}'`)
+    expect(connections).toContain(`Sending message '${message}'`)
+    expect(connections).toContain(`Received message '${message}'`)
+  })
+})
