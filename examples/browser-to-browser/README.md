eafad87a5f3c7281e4b03524641673e7e5cdbe43
David DiMaria <info@collectivesessions.com>
2023-02-17 16:32:10 -0700

    Complete the browser-to-browser example

--- examples/browser-to-browser/README.md
+++ examples/browser-to-browser/README.md
@@ -1,16 +1,22 @@
-# js-libp2p-webrtc Browser to Server
+# js-libp2p-webrtc Browser to Browser
 
 This example leverages the [vite bundler](https://vitejs.dev/) to compile and serve the libp2p code in the browser. You can use other bundlers such as Webpack, but we will not be covering them here.
 
-## Running a relay
+## Running the Relay Server
 
-To run the Go LibP2P relay:
+For browsers to communicate, we first need to run the Go LibP2P relay server:
 
 ```shell
 npm run go-relay
 ```
 
-Copy one of the multiaddress in the output.
+Alternatively, a NodeJS relay server is available:
+
+```shell
+npm run node-relay
+```
+
+Copy one of the multiaddresses in the output.
 
 ## Running the Example
 
@@ -20,15 +26,38 @@ In a separate console tab, install dependencies and start the Vite server:
 npm i && npm run start
 ```
 
-The browser window will automatically open.
-Using the copied multiaddress from the Go server, paste it into the `Server MultiAddress` input and click the `Connect` button.
-Once the peer is connected, click the message section will appear.  Enter a message and click the `Send` button.
+The browser window will automatically open.  Let's call this `Browser A`.
+Using the copied multiaddress from the Go or NodeJS relay server, paste it into the `Remote MultiAddress` input and click the `Connect` button.
+`Browser A` is now connected to the relay server.
+Copy the multiaddress located after the `Listening on` message.
+
+Now open a second browser with the url `http://localhost:5173/`.  Let's call this `Browser B`.
+Using the copied multiaddress from `Listening on` section in `Browser A`, paste it into the `Remote MultiAddress` input and click the `Connect` button.
+`Browser B` is now connected to `Browser A`.
+Copy the multiaddress located after the `Listening on` message.
+
+Using the copied multiaddress from `Listening on` section in `Browser B`, paste it into the `Remote MultiAddress` input in `Browser A` and click the `Connect` button.
+`Browser A` is now connected to `Browser B`.
+
+The peers are now connected to each other.  Enter a message and click the `Send` button in either/both browsers and see the echo'd messages.
 
 The output should look like:
 
+`Browser A`
 ```text
-Dialing /ip4/10.0.1.5/udp/54375/webrtc/certhash/uEiADy8JubdWrAzseyzfXFyCpdRN02eWZg86tjCrTCA5dbQ/p2p/12D3KooWEG7N4bnZfFBNZE7WG6xm2P4Sr6sonMwyD4HCAqApEthb
-Peer connected '/ip4/10.0.1.5/udp/54375/webrtc/certhash/uEiADy8JubdWrAzseyzfXFyCpdRN02eWZg86tjCrTCA5dbQ/p2p/12D3KooWEG7N4bnZfFBNZE7WG6xm2P4Sr6sonMwyD4HCAqApEthb'
-Sending message 'hello'
-Received message 'hello'
+Dialing '/ip4/127.0.0.1/tcp/57708/ws/p2p/12D3KooWRqAUEzPwKMoGstpfJVqr3aoinwKVPu4DLo9nQncbnuLk'
+Listening on /ip4/127.0.0.1/tcp/57708/ws/p2p/12D3KooWRqAUEzPwKMoGstpfJVqr3aoinwKVPu4DLo9nQncbnuLk/p2p-circuit/p2p/12D3KooW9wFiWFELqGJTbzEwtByXsPiHJdHB8n7Kin71VMYyERmC/p2p-webrtc-direct/p2p/12D3KooW9wFiWFELqGJTbzEwtByXsPiHJdHB8n7Kin71VMYyERmC
+Dialing '/ip4/127.0.0.1/tcp/57708/ws/p2p/12D3KooWRqAUEzPwKMoGstpfJVqr3aoinwKVPu4DLo9nQncbnuLk/p2p-circuit/p2p/12D3KooWBZyVLJfQkofqLK4op9TPkHuUumCZt1ybQrPvNm7TVQV9/p2p-webrtc-direct/p2p/12D3KooWBZyVLJfQkofqLK4op9TPkHuUumCZt1ybQrPvNm7TVQV9'
+Sending message 'helloa'
+Received message 'helloa'
+Received message 'hellob'
+```
+
+`Browser B`
+```text
+Dialing '/ip4/127.0.0.1/tcp/57708/ws/p2p/12D3KooWRqAUEzPwKMoGstpfJVqr3aoinwKVPu4DLo9nQncbnuLk/p2p-circuit/p2p/12D3KooW9wFiWFELqGJTbzEwtByXsPiHJdHB8n7Kin71VMYyERmC/p2p-webrtc-direct/p2p/12D3KooW9wFiWFELqGJTbzEwtByXsPiHJdHB8n7Kin71VMYyERmC'
+Listening on /ip4/127.0.0.1/tcp/57708/ws/p2p/12D3KooWRqAUEzPwKMoGstpfJVqr3aoinwKVPu4DLo9nQncbnuLk/p2p-circuit/p2p/12D3KooWBZyVLJfQkofqLK4op9TPkHuUumCZt1ybQrPvNm7TVQV9/p2p-webrtc-direct/p2p/12D3KooWBZyVLJfQkofqLK4op9TPkHuUumCZt1ybQrPvNm7TVQV9
+Received message 'helloa'
+Sending message 'hellob'
+Received message 'hellob'
 ```
--- examples/browser-to-browser/index.js
+++ examples/browser-to-browser/index.js
@@ -1,23 +1,26 @@
-import { multiaddr } from '@multiformats/multiaddr'
-import { pipe } from "it-pipe";
-import { fromString, toString } from "uint8arrays";
-import { webRTCDirect } from 'js-libp2p-webrtc'
-import { webSockets } from '@libp2p/websockets'
-import * as filters from '@libp2p/websockets/filters'
-import { pushable } from 'it-pushable';
-import { mplex } from '@libp2p/mplex'
-import { createLibp2p } from 'libp2p';
-import { noise } from '@chainsafe/libp2p-noise';
+import { multiaddr } from "@multiformats/multiaddr"
+import { pipe } from "it-pipe"
+import { fromString, toString } from "uint8arrays"
+import { webRTCDirect } from "js-libp2p-webrtc"
+import { webSockets } from "@libp2p/websockets"
+import * as filters from "@libp2p/websockets/filters"
+import { pushable } from "it-pushable"
+import { mplex } from "@libp2p/mplex"
+import { createLibp2p } from "libp2p"
+import { noise } from "@chainsafe/libp2p-noise"
 
-let stream;
-const output = document.getElementById('output')
-const sendSection = document.getElementById('send-section')
+// singletons
+let outgoing_stream
+let webrtcDirectAddress
+
+const output = document.getElementById("output")
+const sendSection = document.getElementById("send-section")
 const appendOutput = (line) => {
   const div = document.createElement("div")
   div.appendChild(document.createTextNode(line))
   output.append(div)
 }
-const clean = (line) => line.replaceAll('\n', '')
+const clean = (line) => line.replaceAll("\n", "")
 const sender = pushable()
 
 const node = await createLibp2p({
@@ -40,27 +43,39 @@ const node = await createLibp2p({
 await node.start()
 
 // handle the echo protocol
-await node.handle('/echo/1.0.0', ({ stream }) => {
-  console.log('handling stream: ', stream)
-  void pipe(stream, async function * (source) {
-    for await (const buf of source) {
-      const incoming = toString(buf.subarray())
-      appendOutput('Received from remote: ' + incoming)
-      yield buf
-    }
-  },stream)
+await node.handle("/echo/1.0.0", ({ stream }) => {
+  pipe(
+    stream,
+    async function* (source) {
+      for await (const buf of source) {
+        const incoming = toString(buf.subarray())
+        appendOutput(`Received message '${clean(incoming)}'`)
+        yield buf
+      }
+    },
+    stream
+  )
 })
 
-node.peerStore.addEventListener('change:multiaddrs', (event) => {
+node.peerStore.addEventListener("change:multiaddrs", (event) => {
   const { peerId } = event.detail
+
   if (node.getMultiaddrs().length === 0 || !node.peerId.equals(peerId)) {
     return
   }
+
   node.getMultiaddrs().forEach((ma) => {
     if (ma.protoCodes().includes(290)) {
-      const webrtcDirectAddress = ma.encapsulate(multiaddr(`/p2p-webrtc-direct/p2p/${node.peerId}`))
-      appendOutput(`Listening on ${webrtcDirectAddress}`)
-      sendSection.style.display = 'block'
+      const newWebrtcDirectAddress = ma.encapsulate(
+        multiaddr(`/p2p-webrtc-direct/p2p/${node.peerId}`)
+      )
+
+      // only update if the address is new
+      if (newWebrtcDirectAddress?.toString() !== webrtcDirectAddress?.toString()) {
+        appendOutput(`Listening on ${newWebrtcDirectAddress}`)
+        sendSection.style.display = "block"
+        webrtcDirectAddress = newWebrtcDirectAddress
+      }
     }
   })
 })
@@ -69,18 +84,19 @@ window.connect.onclick = async () => {
   const ma = multiaddr(window.peer.value)
   appendOutput(`Dialing '${ma}'`)
   const connection = await node.dial(ma)
-  console.log('dial completed')
+
   if (!ma.protoCodes().includes(276)) {
     return
   }
-  stream = await connection.newStream(['/echo/1.0.0'])
-  pipe(sender, stream, async (src) => {
-    for await(const buf of src) {
+
+  outgoing_stream = await connection.newStream(["/echo/1.0.0"])
+
+  pipe(sender, outgoing_stream, async (src) => {
+    for await (const buf of src) {
       const response = toString(buf.subarray())
       appendOutput(`Received message '${clean(response)}'`)
     }
   })
-  console.log('created stream', stream)
 }
 
 window.send.onclick = async () => {
