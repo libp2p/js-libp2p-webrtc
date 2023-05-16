79674e61ee7a7409341c6354ae0cf9c099badbc1
chad <chad.nehemiah94@gmail.com>
2023-05-16 10:53:47 -0500

    test: added compliance tests (#132)

--- test/compliance.spec.ts
+++ test/compliance.spec.ts
@@ -1,49 +1,44 @@
 /* eslint-env mocha */
 
-import { multiaddr } from '@multiformats/multiaddr'
+import { mockRegistrar, mockUpgrader } from '@libp2p/interface-mocks'
 import tests from '@libp2p/interface-transport-compliance-tests'
-import sinon from 'sinon'
-import net from 'net'
 import { createEd25519PeerId } from '@libp2p/peer-id-factory'
-
-import { webRTCDirect } from '../src/index.js'
-import { WebRTCDirectTransportComponents } from '../src/transport.js'
+import { multiaddr } from '@multiformats/multiaddr'
+import sinon from 'sinon'
+import { stubInterface } from 'sinon-ts'
+import { webRTC } from '../src/index.js'
+import type { WebRTCTransportComponents } from '../src/private-to-private/transport.js'
+import type { Ed25519PeerId } from '@libp2p/interface-peer-id'
+import type { TransportManager } from '@libp2p/interface-transport'
 
 describe('interface-transport compliance', () => {
-  let components: WebRTCDirectTransportComponents
+  let components: WebRTCTransportComponents
+
+  let relayPeerId: Ed25519PeerId
 
   before(async () => {
+    relayPeerId = await createEd25519PeerId()
     components = {
-      peerId: await createEd25519PeerId()
+      peerId: relayPeerId,
+      registrar: mockRegistrar(),
+      upgrader: mockUpgrader(),
+      transportManager: stubInterface<TransportManager>()
     }
   })
 
   tests({
     async setup () {
-      const transport = webRTCDirect()(components)
+      const transport = webRTC()(components)
+
+      const browserPeerId = await createEd25519PeerId()
 
       const addrs = [
-        multiaddr('/ip4/127.0.0.1/udp/9091/webrtc-direct'),
-        multiaddr('/ip4/127.0.0.1/udp/9092/webrtc-direct'),
-        multiaddr('/ip4/127.0.0.1/udp/9093/webrtc-direct'),
-        multiaddr('/ip4/127.0.0.1/udp/9094/webrtc-direct')
+        multiaddr(`/ip4/127.0.0.1/udp/9091/certhash/uEiAUqV7kzvM1wI5DYDc1RbcekYVmXli_Qprlw3IkiEg6tQ/p2p/${relayPeerId.toString()}/p2p-circuit/webrtc/p2p/${browserPeerId.toString()}`)
       ]
 
       // Used by the dial tests to simulate a delayed connect
       const connector = {
-        delay (delayMs: number) {
-          const netConnect = net.connect
-          sinon.replace(net, 'connect', (opts: any) => {
-            const socket = netConnect(opts)
-            const socketEmit = socket.emit.bind(socket)
-            sinon.replace(socket, 'emit', (...args: [string]) => {
-              const time = args[0] === 'connect' ? delayMs : 0
-              setTimeout(() => socketEmit(...args), time)
-              return true
-            })
-            return socket
-          })
-        },
+        delay () {},
         restore () {
           sinon.restore()
         }
